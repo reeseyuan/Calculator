04981861fc84b009baf5a93c850788241174cfc6
reeseyuan <yuanruisi0119@gmail.com>
2018-03-07 23:42:53 +0800

    Calculator
    First day

---
+++ app/src/main/java/com/example/reese/calculator/CalculatorActivity.java
@@ -0,0 +1,21 @@
+package com.example.reese.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button input,C,division,multiplication,delete,addition,subtraction,equal;
+    Button num1,num2,num3,num4,num5,num6,num7,num8,num9,num0;
+
+    public void
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+    }
+}
